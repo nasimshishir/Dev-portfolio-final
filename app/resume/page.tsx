82e2e4a5970ec2e44fed450a8f0229159433466a
nasimshishir <tech17196@gmail.com>
2024-06-20 11:42:28 +0600

    Services Page Designed

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import { SiTailwindcss, SiNextdotjs, SiNestjs } from 'react-icons/si';
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, FaPhp } from 'react-icons/fa';
+import { DiMongodb, DiMysql, DiPostgresql, DiDocker, DiNpm, DiGithub } from 'react-icons/di';
 
 const Resume = () => {
     return (
--- data/index.ts
+++ data/index.ts
@@ -44,12 +44,62 @@ export const stats = [
 
 export const services = [
     {
-        name: "Web Development",
+        title: "Web Development",
         description: "I build websites and web applications using the latest technologies. I use React, Next.js, and Gatsby to build static websites and apps. I also use Node.js, Express.js, and MongoDB to build APIs and backend services.",
-        image: "/images/services/web-development.png"
+        num: '01',
+        href: "/services/web-development"
     },
     {
-        name: "Mobile Development",
-        description: "I build mobile apps using React Native and Ex"
+        title: "Mobile App Development",
+        description: "I build cross-platform mobile apps using React Native and Expo. I also use the Flutter framework to build mobile apps.",
+        num: '02',
+        href: "/services/mobile-app-development"
+    },
+    {
+        title: "Design",
+        description: "I design websites and mobile apps using Figma and Adobe XD. I also use Adobe Photoshop and Illustrator to create graphics.",
+        num: '03',
+        href: "/services/design"
+    },
+    {
+        title: "Data Science",
+        description: "I use Python, JavaScript, and SQL to build data analysis and machine learning models. I also use the NumPy, Pandas, and SciKit-Learn libraries to build models.",
+        num: '04',
+        href: "/services/data-science"
     }
 ]
+
+export const about = {
+    title: "About me",
+    description: "I'm a full stack developer with a passion for building websites and apps. I'm currently working at a small web development agency.",
+    info: [
+        {
+            fieldName: "Name",
+            fieldValue: "Md Nasim us Sabah Shishir"
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "+8801711211131"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "4+ Years"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "contact.nasimshishir@gmail.com"
+        },
+        {
+            fieldName: "Nationality",
+            fieldValue: "Bangladeshi"
+        },
+        {
+            fieldName: "Languages",
+            fieldValue: "Bangla, English, Hindi, Spanish, Polish"
+        },
+    ]
+}
+
+export const experiences = {
+    icon: ''
+}
