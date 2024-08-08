aa143ea15c69403570923e774bd39c154ed92c01
nasimshishir <tech17196@gmail.com>
2024-08-08 14:25:29 +0600

    Booking button added to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Photo from '@/components/Photo'
 import Stats from '@/components/Stats'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
+import { FaVideo } from "react-icons/fa"
 
 const Homepage = () => {
   return (
@@ -18,7 +19,7 @@ const Homepage = () => {
             <p className='max-w-[500px] mb-9 text-white/80'>
               I am a passionate Full-Stack Web Developer with 3+ Years of Expertise in WordPress, E-commerce Solutions and fullstack development.
             </p>
-            <div className='flex flex-col xl:flex-row items-center gap-8'>
+            <div className='flex flex-col xl:flex-row items-center gap-4'>
               <Link
                 href="/cv.pdf"
                 target="_blank"
@@ -29,9 +30,18 @@ const Homepage = () => {
                   <FiDownload className='text-xl ml-2' />
                 </Button>
               </Link>
-              <div className='mb-8 xl:mb-0'>
-                <Socials containerStyles='flex gap-6' iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base  hover:bg-accent hover:text-primary hover:transition-all duration-500' />
-              </div>
+              <Link
+                href="https://cal.com/nasimshishir/30min"
+                target="_blank"
+              >
+                <Button className='uppercase flex items-center gap-2 transition-all duration-200' variant={'outline'} size={'lg'} >
+                  <span>Book a Meeting</span>
+                  <FaVideo className='text-xl ml-2' />
+                </Button>
+              </Link>
+            </div>
+            <div className='flex justify-center xl:justify-start gap-8 xl:gap-12 my-8'>
+              <Socials containerStyles='flex gap-6' iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base  hover:bg-accent hover:text-primary hover:transition-all duration-500' />
             </div>
           </div>
           <div className='order-1 xl:order-none mb-8 xl:mb-0'>
